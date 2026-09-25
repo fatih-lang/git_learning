ac11fe2b807c76b10a30ed23e331e338472a779d
Fatih <fatihkasozi20@gmail.com>
2026-09-25 23:24:48 +0200

    Change greeting on main

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # This is our first Python program for learning Git.
 
-print("Hello, Git!")
+print("Hello from the main branch!")
 print("I am learning version control.")
 print("I am learning backend")
 
