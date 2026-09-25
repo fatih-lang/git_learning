97c3e4961723195ab594841c38f1504960f51ad9
Fatih <fatihkasozi20@gmail.com>
2026-09-25 17:55:56 +0200

    Connect project workflow to GitHub

--- hello.py
+++ hello.py
@@ -2,4 +2,7 @@
 
 print("Hello, Git!")
 print("I am learning version control.")
-print("I am learning backend.")
+print("I am learning backend")
+
+# This line demonstrates another change that we can save in Git history.
+print("My project is now connected to GitHub!")
