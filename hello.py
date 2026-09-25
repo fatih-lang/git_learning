7e13b11198c2b172aa9d4e46581a8956602c87eb
Fatih <fatihkasozi20@gmail.com>
2026-09-25 23:20:12 +0200

    Change greeting for conflict demo

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 # This is our first Python program for learning Git.
 
-print("Hello, Git!")
+print("Hello from the conflict branch!")
 print("I am learning version control.")
 print("I am learning backend")
 
